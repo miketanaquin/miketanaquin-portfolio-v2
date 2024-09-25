8b62c6005852f60195d74d53ab9dedbc8d14bb9f
miketanaquin <137193747+miketanaquin@users.noreply.github.com>
2024-09-25 11:26:03 +0800

    changed name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mike Tanaquin Portfolio",
+  title: "Mike Tanaquin's Portfolio",
   description: "Check out my Digital Space",
 };
 
