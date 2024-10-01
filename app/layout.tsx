5ba1c4848400c164f072fb5ca345e11064276745
miketanaquin <137193747+miketanaquin@users.noreply.github.com>
2024-10-01 14:51:30 +0800

    added seo desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mike Tanaquin's Portfolio",
-  description: "Check out my Digital Space",
+  description: "Mike Louie O Tañaquin – A software engineer with expertise in both frontend and backend development. Specializing in React JS, Vue JS, Tailwind CSS, Bootstrap for the frontend, and PHP, Laravel for the backend. With over a year of experience, Mike has developed and collaborated on diverse projects, including e-commerce platforms, exam application systems, and data management solutions. Based in the Philippines, Mike creates dynamic, responsive, and scalable web applications that prioritize user experience and SEO optimization. Explore Mike's portfolio to see his innovative solutions in action.",
 };
 
 export default function RootLayout({
